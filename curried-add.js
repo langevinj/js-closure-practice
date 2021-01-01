114617380b07c42f5add78b16039d403eb6fd9ab
Jeremy Langevin <langevinj832@gmail.com>
2021-01-01 10:08:20 -0500

    Completed curried-add

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,18 @@
 function curriedAdd(total) {
+    let currTotal = total || 0;
 
+    if(currTotal === 0) return currTotal;
+
+    let sum = total;
+
+    return function add(){
+        if(arguments[0]){
+            sum += arguments[0]
+            return add
+        } else {
+            return sum;
+        }
+    }
 }
 
 module.exports = { curriedAdd };
