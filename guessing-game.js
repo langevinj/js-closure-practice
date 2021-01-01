ea3fe0a964882e2c5707b29b138079406f210c89
Jeremy Langevin <langevinj832@gmail.com>
2021-01-01 08:07:57 -0500

    Completed guessing-game

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,18 @@
 function guessingGame() {
-
+    let count = 0;
+    let randomNum = Math.floor(Math.random() * (99 - 0 + 1));
+    let won = false;
+    
+    return function guess(num) {
+        count++;
+        if(won) return "The game is over, you already won!"
+        if(randomNum === num){
+            won = true;
+            return `You win! You found ${randomNum} in ${count} guesses.`
+        } 
+        if(randomNum > num) return `${num} is too low!`
+        if(randomNum < num) return `${num} is too high!`
+    };
 }
 
 module.exports = { guessingGame };
